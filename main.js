4c713ed8ca64c770658235d6d5ede2a2b308bcd4
Detreyl <75556219+Detreyl@users.noreply.github.com>
2024-05-16 09:55:08 -0700

    Initial files

---
+++ main.js
@@ -0,0 +1,6 @@
+//
+// Place any custom JS here
+//
+//initializes tooltip functionality
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
